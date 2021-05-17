fb5d4929a4eb7bc505e332d76e68a04b75d2466b
Jesper <jesperfriberg94@hotmail.com>
2021-05-17 14:44:11 +0200

    adding controllers

--- src/main/java/com/example/almgroupproject/controller/DirectorController.java
+++ src/main/java/com/example/almgroupproject/controller/DirectorController.java
@@ -3,11 +3,9 @@ package com.example.almgroupproject.controller;
 /**
  * Created by Jesper Friberg Spång
  * Date: 2021-05-17
- * Time: 14:32
+ * Time: 14:43
  * Project: almGroupProject
  * Copyright: MIT
  */
-
-public class ActorController {
-
+public class DirectorController {
 }
