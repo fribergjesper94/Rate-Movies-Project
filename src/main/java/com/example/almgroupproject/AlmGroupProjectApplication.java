39856e480e96a9e0a3271effcf9c507f0c486d13
Jesper <jesperfriberg94@hotmail.com>
2021-05-17 13:32:04 +0200

    starting project

---
+++ src/main/java/com/example/almgroupproject/AlmGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.almgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlmGroupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/almgroupproject/AlmGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.almgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmGroupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
