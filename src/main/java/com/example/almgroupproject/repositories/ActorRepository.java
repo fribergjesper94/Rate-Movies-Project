75b578960c0bd0f54d51596d8135b390464ee790
Jesper <jesperfriberg94@hotmail.com>
2021-05-17 15:05:14 +0200

    added repos

---
+++ src/main/java/com/example/almgroupproject/repositories/ActorRepository.java
@@ -0,0 +1,11 @@
+package com.example.almgroupproject.repositories;
+
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2021-05-17
+ * Time: 15:00
+ * Project: almGroupProject
+ * Copyright: MIT
+ */
+public interface ActorRepository {
+}
