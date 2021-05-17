78f8b36ab5c632faa32c0cb73f185e59d9da46a2
Jesper <jesperfriberg94@hotmail.com>
2021-05-17 15:04:33 +0200

    added services

---
+++ src/main/java/com/example/almgroupproject/service/ActorService.java
@@ -0,0 +1,16 @@
+package com.example.almgroupproject.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2021-05-17
+ * Time: 14:57
+ * Project: almGroupProject
+ * Copyright: MIT
+ */
+@Service
+public class ActorService {
+    private final ActorRepository repository;
+
+}
