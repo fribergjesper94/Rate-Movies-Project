e79b7f4b4177fb0007c9b0b996b3c669154c4628
Jesper <jesperfriberg94@hotmail.com>
2021-05-17 14:39:12 +0200

    added controller class

---
+++ src/main/java/com/example/almgroupproject/controller/ActorController.java
@@ -0,0 +1,12 @@
+package com.example.almgroupproject.controller;
+
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2021-05-17
+ * Time: 14:32
+ * Project: almGroupProject
+ * Copyright: MIT
+ */
+public class ActorController {
+
+}
