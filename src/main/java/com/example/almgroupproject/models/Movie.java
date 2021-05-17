e0ccbac6bc11ce48d50b8781a9d894ab76761baa
Jesper <jesperfriberg94@hotmail.com>
2021-05-17 14:09:00 +0200

    changed folder structure

---
+++ src/main/java/com/example/almgroupproject/models/Movie.java
@@ -0,0 +1,17 @@
+package com.example.almgroupproject.models;
+
+/**
+ * Created by Jesper Friberg Spång
+ * Date: 2021-05-17
+ * Time: 14:04
+ * Project: almGroupProject
+ * Copyright: MIT
+ */
+
+
+public class Movie {
+
+    private String title;
+
+
+}
