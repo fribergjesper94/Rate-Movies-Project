bf5a772617ab36376cb1e4157475ec063593dd3f
Elliot Åberg Fält <elliot.falt@gmail.com>
2021-05-18 09:38:57 +0200

    Lade till alla DirectorRepository och fixade alla andra director klasser

--- src/main/java/com/example/almgroupproject/controller/DirectorController.java
+++ src/main/java/com/example/almgroupproject/controller/DirectorController.java
@@ -1,5 +1,15 @@
 package com.example.almgroupproject.controller;
 
+import com.example.almgroupproject.models.Director;
+import com.example.almgroupproject.service.DirectorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created by Jesper Friberg Spång
  * Date: 2021-05-17
@@ -7,5 +17,19 @@ package com.example.almgroupproject.controller;
  * Project: almGroupProject
  * Copyright: MIT
  */
+@RestController("director")
+@RequiredArgsConstructor
 public class DirectorController {
+
+    private final DirectorService service;
+
+    @GetMapping(path = "/all")
+    public List<Director> allDirectors() {
+        return service.getAllDirectors();
+    }
+
+    @PostMapping(path = "/add")
+    public String addDirector(@RequestBody Director director) {
+        return service.save(director);
+    }
 }
--- src/main/java/com/example/almgroupproject/service/DirectorService.java
+++ src/main/java/com/example/almgroupproject/service/DirectorService.java
@@ -1,5 +1,12 @@
 package com.example.almgroupproject.service;
 
+import com.example.almgroupproject.models.Director;
+import com.example.almgroupproject.repositories.DirectorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by Jesper Friberg Spång
  * Date: 2021-05-17
@@ -7,5 +14,22 @@ package com.example.almgroupproject.service;
  * Project: almGroupProject
  * Copyright: MIT
  */
+@Service
+@RequiredArgsConstructor
 public class DirectorService {
+    private final DirectorRepository repository;
+
+    public List<Director> getAllDirectors() {
+        return repository.findAll();
+    }
+
+    public String save(Director director) {
+
+        if (repository.findDirectorByFirstNameAndLastName(director.getFirstName(), director.getLastName()) != null) {
+            return "Director already exists";
+        }
+
+        repository.save(director);
+        return "Director saved";
+    }
 }
