ea2caf2413f3e3bdff9c1fdf80cacafc2b16f7cc
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2021-05-18 09:25:54 +0200

    added repo and code in service/controller

--- src/main/java/com/example/almgroupproject/controller/ReviewController.java
+++ src/main/java/com/example/almgroupproject/controller/ReviewController.java
@@ -1,5 +1,15 @@
 package com.example.almgroupproject.controller;
 
+import com.example.almgroupproject.models.Review;
+import com.example.almgroupproject.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created by Jesper Friberg Spång
  * Date: 2021-05-17
@@ -7,6 +17,21 @@ package com.example.almgroupproject.controller;
  * Project: almGroupProject
  * Copyright: MIT
  */
+@RestController("review")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService service;
+
+    @GetMapping(path = "/all")
+    public List<Review> allReviews () {
+
+        return service.getAllReviews();
+    }
+
+    @PostMapping(path = "/add")
+    public String addReview (@RequestBody Review review) {
+        return service.save(review);
+    }
 
 }
