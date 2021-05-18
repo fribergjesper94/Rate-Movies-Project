5ac35c16ceb6b99bfb7e6c1c8bd419a801ae6655
Ulf <72010004+UlfNyberg@users.noreply.github.com>
2021-05-18 10:57:50 +0200

    Feature/review basic functionality
    * added repo and code in service/controller
    
    * id added and service corrected
    
    * review controller after add

--- src/main/java/com/example/almgroupproject/service/ReviewService.java
+++ src/main/java/com/example/almgroupproject/service/ReviewService.java
@@ -1,5 +1,12 @@
 package com.example.almgroupproject.service;
 
+import com.example.almgroupproject.models.Review;
+import com.example.almgroupproject.repositories.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by Jesper Friberg Spång
  * Date: 2021-05-17
@@ -7,5 +14,23 @@ package com.example.almgroupproject.service;
  * Project: almGroupProject
  * Copyright: MIT
  */
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewRepository repository;
+
+    public List<Review> getAllReviews () {
+        return repository.findAll();
+    }
+
+
+    public String save(Review review) {
+
+        if (repository.findReviewById(review.getId()) != null) {
+            return "Review already exists";
+        }
+        repository.save(review);
+        return "Review saved";
+    }
 }
