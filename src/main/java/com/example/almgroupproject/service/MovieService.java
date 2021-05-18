12b0e80e42d2298875831e6316582b3266acf7a4
Peter <peter.pilestedt@gmail.com>
2021-05-18 14:26:19 +0200

    addActorToMovie method added

--- src/main/java/com/example/almgroupproject/service/MovieService.java
+++ src/main/java/com/example/almgroupproject/service/MovieService.java
@@ -1,6 +1,8 @@
 package com.example.almgroupproject.service;
 
+import com.example.almgroupproject.models.Actor;
 import com.example.almgroupproject.models.Movie;
+import com.example.almgroupproject.repositories.ActorRepository;
 import com.example.almgroupproject.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,14 +21,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieService {
 
-    private final MovieRepository repository;
+    private final MovieRepository movieRepository;
+    private final ActorRepository actorRepository;
 
     public List<Movie> getAllMovies(){
-        return repository.findAll();
+        return movieRepository.findAll();
     }
 
     public String save(Movie movie) {
-        repository.save(movie);
+        movieRepository.save(movie);
         return "Movie saved";
+    }
+
+    public String addActorToMovie(String title, String firstname, String lastname) {
+        Actor selectedActor = actorRepository.findActorByFirstNameAndLastName(firstname,lastname);
+        Movie selectedMovie = movieRepository.findMovieByTitle(title);
+
+        if(selectedActor == null){
+            return "Actor not found";
+        }else if (selectedMovie == null){
+            return "Movie not found";
+        }else if(selectedMovie.getActorList().contains(selectedActor)){
+            return "Actor already in movie";
+        }
+        
+        selectedMovie.addActorToList(selectedActor);
+        movieRepository.save(selectedMovie);
+
+        return "Actor added to movie";
     }
 }
