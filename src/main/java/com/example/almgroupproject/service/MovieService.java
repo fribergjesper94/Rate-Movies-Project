ec53e64b374374d2ca4af822ba3b7f400b5cd71b
Peter <peter.pilestedt@gmail.com>
2021-05-18 10:06:31 +0200

    Adds movie classes

--- src/main/java/com/example/almgroupproject/service/MovieService.java
+++ src/main/java/com/example/almgroupproject/service/MovieService.java
@@ -1,5 +1,12 @@
 package com.example.almgroupproject.service;
 
+import com.example.almgroupproject.models.Movie;
+import com.example.almgroupproject.repositories.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by Jesper Friberg Spång
  * Date: 2021-05-17
@@ -7,5 +14,15 @@ package com.example.almgroupproject.service;
  * Project: almGroupProject
  * Copyright: MIT
  */
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+
+    private final MovieRepository repository;
+
+    public List<Movie> getAllMovies(){
+        return repository.findAll();
+    }
+
 }
